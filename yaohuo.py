be3bff06dd385dd8aeed7627d3d42b2253e17265
PsS8NrHWp <66151005+BaeKey@users.noreply.github.com>
2023-09-03 17:02:35 +0800

    Update yaohuo.py

--- yaohuo.py
+++ yaohuo.py
@@ -10,15 +10,17 @@ token = "xxxxx"
 userid = "xxxxx"
 # 替换为自己的sidyaohuo
 sidyaohuo = "xxxxx"
+# 刷新间隔/s
+refresh_time = 60
 
 # # 登录用户名
 # username = ""
 # # 登录密码
 # password = ""
 
-
 headers = {
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+    'Accept':
+    'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
     'Accept-Language': 'zh-CN,zh;q=0.9',
     'Cache-Control': 'max-age=0',
     'Connection': 'keep-alive',
@@ -28,16 +30,15 @@ headers = {
     'Sec-Fetch-Site': 'same-origin',
     'Sec-Fetch-User': '?1',
     'Upgrade-Insecure-Requests': '1',
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36',
-    'sec-ch-ua': '"Chromium";v="116", "Not)A;Brand";v="24", "Google Chrome";v="116"',
+    'User-Agent':
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36',
+    'sec-ch-ua':
+    '"Chromium";v="116", "Not)A;Brand";v="24", "Google Chrome";v="116"',
     'sec-ch-ua-mobile': '?0',
     'sec-ch-ua-platform': '"Windows"',
 }
 
-cookies = {
-      'sidyaohuo': sidyaohuo
-}
-
+cookies = {'sidyaohuo': sidyaohuo}
 
 # # 登录获取cookies
 # def get_cookies():
@@ -55,18 +56,34 @@ cookies = {
 #     cookies=requests.post(url,headers=headers,data=data).cookies
 #     return cookies
 
-# 获取帖子内容
+
+# 获取帖子内容并转换为Markdown格式
 def get_content(url):
     try:
-        response = requests.get(url, headers=headers,cookies=cookies)
-        yaohuo_content = etree.HTML(response.content).xpath('//div[@class="bbscontent"]/text()')
-        return yaohuo_content
+        response = requests.get(url, headers=headers, cookies=cookies)
+        yaohuo_content = etree.HTML(response.content).xpath(
+            '//div[@class="bbscontent"]//comment()[contains(., "listS")]/following-sibling::node()[following::comment()[contains(., "listE")]]'
+        )
+        # 初始化一个Markdown字符串
+        markdown_content = ""
+        for element in yaohuo_content:
+            if isinstance(element, str):
+                markdown_content += element
+            elif element.tag == "br":
+                markdown_content += "\n"
+
+        return markdown_content
+
     except Exception as e:
         print("获取内容失败：", e)
 
-# TG推送
+
+# 发送消息
 def send_message(chat_id, text):
-    headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36'}
+    headers = {
+        'user-agent':
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36'
+    }
     text = parse.quote(text)
     post_url = f'https://api.telegram.org/bot{token}/sendMessage?parse_mode=MarkdownV2&chat_id={chat_id}&text={text}'
     try:
@@ -79,6 +96,7 @@ def send_message(chat_id, text):
         except Exception:
             pass
 
+
 # 主程序
 def main():
     yaohuo_list = set()
@@ -86,11 +104,16 @@ def main():
 
     while True:
         try:
-            response = requests.get(url_yaohuo, headers=headers, cookies=cookies)
+            response = requests.get(url_yaohuo,
+                                    headers=headers,
+                                    cookies=cookies)
             xml_content = etree.HTML(response.content)
-            href_list = xml_content.xpath('//div[contains(@class, "listdata line")]/a[1]/@href')
-            reply_number = xml_content.xpath('//div[contains(@class, "listdata line")]/a[2]/text()')
-            href = xml_content.xpath('//div[contains(@class, "listdata line")]/a[1]/text()')
+            href_list = xml_content.xpath(
+                '//div[contains(@class, "listdata line")]/a[1]/@href')
+            reply_number = xml_content.xpath(
+                '//div[contains(@class, "listdata line")]/a[2]/text()')
+            href = xml_content.xpath(
+                '//div[contains(@class, "listdata line")]/a[1]/text()')
 
             for i in range(len(reply_number)):
                 if reply_number[i] == '0':
@@ -98,7 +121,8 @@ def main():
                         yaohuo_list.add(str(href[i]))
                         name = href[i]
                         url_list = f"https://yaohuo.me/{href_list[i]}"
-                        current_datetime = time.strftime("%Y/%m/%d  %H:%M:%S", time.localtime())
+                        current_datetime = time.strftime(
+                            "%Y/%m/%d  %H:%M:%S", time.localtime())
                         content = get_content(url_list)
                         text = f'主        题：*[{name}]({url_list})*\n时        间：{current_datetime}\n内容预览：{content}'
                         send_message(userid, text)
@@ -106,10 +130,11 @@ def main():
                         pass
                 else:
                     pass
-            time.sleep(20)
+            time.sleep(refresh_time)
         except Exception as e:
             print("发生异常:", e)
-            time.sleep(150)
+            time.sleep(300)
+
 
 if __name__ == "__main__":
     main()
